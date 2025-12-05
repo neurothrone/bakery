93a0e5b9a190b1e390d23389e6e0d710fcbde377
neurothrone <54468049+neurothrone@users.noreply.github.com>
2025-12-05 10:47:03 +0100

    add database context and customer repository to Program.cs

--- src/Bakery.Api/Program.cs
+++ src/Bakery.Api/Program.cs
@@ -1,6 +1,16 @@
 using Bakery.Api.Endpoints;
+using Bakery.Database.Data;
+using Bakery.Database.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<BakeryDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<CustomerRepository>();
 
 var app = builder.Build();
 
